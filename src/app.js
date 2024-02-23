68112e1e144a599fd5dc4dc5877d66bbf032888e
Bikash Mishra <mishrabikash202@gmail.com>
2024-02-23 23:31:41 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from 'cors'
+
+const app = express();
+
+//  Basic Middlewares
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser());
+app.use(express.static("public"))
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import DB_Connection from './DB/index.js'
 import dotenv from 'dotenv'
+import {app} from './app.js'
 dotenv.config({
     path: './.env'
 })
 
 //DB Connection Method
 DB_Connection()
+.then(()=>{
+    app.listen(process.env.PORT || 2000,()=>console.log(`server is running ${process.env.PORT}`))
+})
+.catch(()=>console.log("Error Occure while connecting to DB"))
